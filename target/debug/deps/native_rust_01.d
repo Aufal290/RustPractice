b38462a7b858ce3c3f4e6c0f9c846777aef67412
Aufal290 <138507284+Aufal290@users.noreply.github.com>
2026-04-23 23:26:10 +0700

    Test

---
+++ target/debug/deps/native_rust_01.d
@@ -0,0 +1,5 @@
+C:\d\@.Freelance\RustPractice\target\debug\deps\native_rust_01.d: native-rust-01\src\main.rs
+
+C:\d\@.Freelance\RustPractice\target\debug\deps\native_rust_01.exe: native-rust-01\src\main.rs
+
+native-rust-01\src\main.rs:
---
+++ target/debug/native-rust-01.d
@@ -0,0 +1,1 @@
+C:\d\@.Freelance\RustPractice\target\debug\native-rust-01.exe: C:\d\@.Freelance\RustPractice\native-rust-01\src\main.rs
